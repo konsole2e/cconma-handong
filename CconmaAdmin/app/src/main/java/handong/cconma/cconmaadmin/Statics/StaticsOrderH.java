d4b15a87a6c8bdbde3a50a54ac8d813c0c21d072
ljs401 <irisluz@naver.com>
2015-07-09 14:34:46 +0900

    7/9 차트 별 세팅 일괄처리

--- CconmaAdmin/app/src/main/java/handong/cconma/cconmaadmin/Statics/StaticsOrderH.java
+++ CconmaAdmin/app/src/main/java/handong/cconma/cconmaadmin/Statics/StaticsOrderH.java
@@ -19,9 +19,8 @@ import handong.cconma.cconmaadmin.R;
 public class StaticsOrderH extends Activity {
     private LineChart pcChart;
     private LineChart mobChart;
-    private ArrayList<String> xVal;
-    private ArrayList<String> yVal;
     private BackPressCloseHandler backPressCloseHandler;
+    private StaticsCommonSetting setting;
     private boolean mode = false;
 
     @Override
@@ -30,14 +29,17 @@ public class StaticsOrderH extends Activity {
         setContentView(R.layout.activity_statics_order_h);
 
         backPressCloseHandler = new BackPressCloseHandler(this);
+        setting = new StaticsCommonSetting();
 
         StaticsOrderHManager manager = new StaticsOrderHManager(this);
 
         pcChart = (LineChart) findViewById(R.id.order_hourly_pc_chart);
         mobChart = (LineChart) findViewById(R.id.order_hourly_mobile_chart);
 
-        pcChart.setDescription("");
-        mobChart.setDescription("");
+        setting.commonSetting(pcChart);
+        setting.commonSetting(mobChart);
+//        pcChart.setDescription("");
+//        mobChart.setDescription("");
 
         pcChart.getLegend().setPosition(Legend.LegendPosition.BELOW_CHART_CENTER);
         pcChart.getLegend().setTextSize(7f);
@@ -45,13 +47,13 @@ public class StaticsOrderH extends Activity {
         mobChart.getLegend().setTextSize(7f);
 
         // scaling can now only be done on x- and y-axis separately
-        pcChart.setPinchZoom(false);
-        mobChart.setPinchZoom(false);
+ //       pcChart.setPinchZoom(false);
+ //       mobChart.setPinchZoom(false);
 
-        pcChart.setDrawGridBackground(true);
-        mobChart.setDrawGridBackground(true);
+ //       pcChart.setDrawGridBackground(true);
+ //       mobChart.setDrawGridBackground(true);
 
-        XAxis xAxis = pcChart.getXAxis();
+ /*       XAxis xAxis = pcChart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setLabelsToSkip(0);
         xAxis.setDrawGridLines(false);
@@ -60,21 +62,22 @@ public class StaticsOrderH extends Activity {
         xAxis2.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis2.setLabelsToSkip(0);
         xAxis2.setDrawGridLines(false);
-
+*/
         //pcChart.getAxisLeft().setDrawGridLines(false);
 //        mobChart.getAxisLeft().setDrawGridLines(false);
 
-        pcChart.getAxisRight().setEnabled(false);
-        mobChart.getAxisRight().setEnabled(false);
+//        pcChart.getAxisRight().setEnabled(false);
+//        mobChart.getAxisRight().setEnabled(false);
 
-        pcChart.getLegend().setEnabled(true);
-        mobChart.getLegend().setEnabled(true);
+//        pcChart.getLegend().setEnabled(true);
+//        mobChart.getLegend().setEnabled(true);
+
+//        pcChart.setDoubleTapToZoomEnabled(false);
+//        mobChart.setDoubleTapToZoomEnabled(false);
 
         pcChart.setData(manager.setting());
         mobChart.setData(manager.setting());
 
-        pcChart.setDoubleTapToZoomEnabled(false);
-        mobChart.setDoubleTapToZoomEnabled(false);
 
         (findViewById(R.id.order_hourly_pc_zoom)).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,7 +85,9 @@ public class StaticsOrderH extends Activity {
                 if (mode == false) {
                     mode = true;
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // 가로전환
-                    pcChart.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+                    setting.zoomSetting(pcChart);
+                  /*  pcChart.setScaleEnabled(true);
+                    pcChart.setPinchZoom(true);*/
                     mobChart.setVisibility(View.GONE);
                     (findViewById(R.id.order_hourly_pc_rl)).setVisibility(View.GONE);
                     (findViewById(R.id.order_hourly_mobile_rl)).setVisibility(View.GONE);
@@ -96,7 +101,8 @@ public class StaticsOrderH extends Activity {
                 if (mode == false) {
                     mode = true;
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // 가로전환
-                    mobChart.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+                    setting.zoomSetting(mobChart);
+                    //mobChart.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
                     pcChart.setVisibility(View.GONE);
                     (findViewById(R.id.order_hourly_pc_rl)).setVisibility(View.GONE);
                     (findViewById(R.id.order_hourly_mobile_rl)).setVisibility(View.GONE);
@@ -120,11 +126,11 @@ public class StaticsOrderH extends Activity {
         public void onBackPressed() {
             Configuration config = getResources().getConfiguration();
             if (mode && config.orientation == Configuration.ORIENTATION_LANDSCAPE) {// 가로
-                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // 가로전환
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // 세로전환
                 mode = false;
                 activity.recreate();
-            } else if (mode && config.orientation == Configuration.ORIENTATION_PORTRAIT) {
-                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // 세로전환
+            } else if (mode && config.orientation == Configuration.ORIENTATION_PORTRAIT) {// 세로
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // 가로전환
                 mode = false;
                 activity.recreate();
             } else {
--- CconmaAdmin/app/src/main/java/handong/cconma/cconmaadmin/Statics/StaticsTrade.java
+++ CconmaAdmin/app/src/main/java/handong/cconma/cconmaadmin/Statics/StaticsTrade.java
@@ -24,6 +24,7 @@ public class StaticsTrade extends Activity {
     private LineChart monthlyChart;
     private boolean mode = false;
     private BackPressCloseHandler backPressCloseHandler;
+    private StaticsCommonSetting setting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +32,7 @@ public class StaticsTrade extends Activity {
         setContentView(R.layout.activity_statics_trade);
 
         backPressCloseHandler = new BackPressCloseHandler(this);
+        setting = new StaticsCommonSetting();
 
         StaticsTradeManager manager = new StaticsTradeManager(this);
 
@@ -39,63 +41,69 @@ public class StaticsTrade extends Activity {
         weeklyChart = (LineChart) findViewById(R.id.trade_weekly_lineChart);
         monthlyChart = (LineChart) findViewById(R.id.trade_monthly_lineChart);
 
-        hourlyChart.setDescription("");
-        dailyChart.setDescription("");
-        weeklyChart.setDescription("");
-        monthlyChart.setDescription("");
+        setting.commonSetting(hourlyChart);
+        setting.commonSetting(dailyChart);
+        setting.commonSetting(weeklyChart);
+        setting.commonSetting(monthlyChart);
 
-        hourlyChart.setDrawOrder(new DrawOrder[]{DrawOrder.BAR, DrawOrder.LINE});
+//        hourlyChart.setDescription("");
+ //       dailyChart.setDescription("");
+//        weeklyChart.setDescription("");
+//        monthlyChart.setDescription("");
+
+//        hourlyChart.setDrawOrder(new DrawOrder[]{DrawOrder.BAR, DrawOrder.LINE});
+/*
         hourlyChart.getLegend().setFormSize(5f);
         hourlyChart.getLegend().setFormToTextSpace(2);
         hourlyChart.getLegend().setTextSize(7f);
         hourlyChart.getLegend().setPosition(Legend.LegendPosition.BELOW_CHART_CENTER);
+*/
 
-        dailyChart.getLegend().setPosition(Legend.LegendPosition.BELOW_CHART_CENTER);
+//        dailyChart.getLegend().setPosition(Legend.LegendPosition.BELOW_CHART_CENTER);
 
-        XAxis xAxisH = hourlyChart.getXAxis();
+/*        XAxis xAxisH = hourlyChart.getXAxis();
         xAxisH.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxisH.setDrawGridLines(false);
-        xAxisH.setLabelsToSkip(0);
+        xAxisH.setLabelsToSkip(0);*/
 
-        XAxis xAxisD = dailyChart.getXAxis();
-        xAxisD.setPosition(XAxis.XAxisPosition.BOTTOM);
+      /*  XAxis xAxisD = dailyChart.getXAxis();
+        xAxisD.setPosition(XAxis.XAxisPosition.BOTTOM);*/
 
-        XAxis xAxisW = weeklyChart.getXAxis();
+/*        XAxis xAxisW = weeklyChart.getXAxis();
         xAxisW.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxisW.setDrawGridLines(false);
 
         XAxis xAxisM = monthlyChart.getXAxis();
         xAxisM.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxisM.setDrawGridLines(false);
+        xAxisM.setDrawGridLines(false);*/
 
-        YAxis rightAxisH = hourlyChart.getAxisRight();
-        rightAxisH.setEnabled(false);
+  /*      YAxis rightAxisH = hourlyChart.getAxisRight();
+        rightAxisH.setEnabled(false);*/
 
-        YAxis rightAxisD = dailyChart.getAxisRight();
-        rightAxisD.setEnabled(false);
+/*        YAxis rightAxisD = dailyChart.getAxisRight();
+        rightAxisD.setEnabled(false);*/
 
-        YAxis leftAxisD = dailyChart.getAxisLeft();
-        leftAxisD.setDrawGridLines(false);
+/*        YAxis leftAxisD = dailyChart.getAxisLeft();
+        leftAxisD.setDrawGridLines(false);*/
 
-        YAxis rightAxisW = weeklyChart.getAxisRight();
-        rightAxisW.setEnabled(false);
+  /*      YAxis rightAxisW = weeklyChart.getAxisRight();
+        rightAxisW.setEnabled(false);*/
 
-        YAxis leftAxisW = weeklyChart.getAxisLeft();
+/*        YAxis leftAxisW = weeklyChart.getAxisLeft();
         //    leftAxisW.setDrawGridLines(false);
 
-        YAxis rightAxisM = monthlyChart.getAxisRight();
-        rightAxisM.setEnabled(false);
+       *//* YAxis rightAxisM = monthlyChart.getAxisRight();
+        rightAxisM.setEnabled(false);*//*
 
         YAxis leftAxisM = monthlyChart.getAxisLeft();
-        //  leftAxisM.setDrawGridLines(false);
+        //  leftAxisM.setDrawGridLines(false);*/
 
         hourlyChart.setData(manager.hourlyChartSetting());
         dailyChart.setData(manager.dailyChartSetting());
-        dailyChart.invalidate();
         weeklyChart.setData(manager.weeklyChartSetting());
-        leftAxisW.addLimitLine(manager.weeklkyAVG());
+        weeklyChart.getAxisLeft().addLimitLine(manager.weeklkyAVG());
         monthlyChart.setData(manager.monthlyChartSetting());
-        leftAxisM.addLimitLine(manager.monthlyAVG());
+        monthlyChart.getAxisLeft().addLimitLine(manager.monthlyAVG());
 
         (findViewById(R.id.trade_hourly_zoom)).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -103,7 +111,8 @@ public class StaticsTrade extends Activity {
                 if (mode == false) {
                     mode = true;
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // 가로전환
-                    hourlyChart.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+                    //hourlyChart.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+                    setting.zoomSetting(hourlyChart);
                     dailyChart.setVisibility(View.GONE);
                     weeklyChart.setVisibility(View.GONE);
                     monthlyChart.setVisibility(View.GONE);
@@ -121,7 +130,8 @@ public class StaticsTrade extends Activity {
                 if (mode == false) {
                     mode = true;
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // 가로전환
-                    dailyChart.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+                    //dailyChart.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+                    setting.zoomSetting(dailyChart);
                     hourlyChart.setVisibility(View.GONE);
                     weeklyChart.setVisibility(View.GONE);
                     monthlyChart.setVisibility(View.GONE);
@@ -139,7 +149,8 @@ public class StaticsTrade extends Activity {
                 if (mode == false) {
                     mode = true;
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // 가로전환
-                    weeklyChart.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+                   // weeklyChart.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+                    setting.zoomSetting(weeklyChart);
                     hourlyChart.setVisibility(View.GONE);
                     dailyChart.setVisibility(View.GONE);
                     monthlyChart.setVisibility(View.GONE);
@@ -157,7 +168,8 @@ public class StaticsTrade extends Activity {
                 if (mode == false) {
                     mode = true;
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // 가로전환
-                    monthlyChart.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+                 //   monthlyChart.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+                   setting.zoomSetting(monthlyChart);
                     hourlyChart.setVisibility(View.GONE);
                     dailyChart.setVisibility(View.GONE);
                     weeklyChart.setVisibility(View.GONE);
