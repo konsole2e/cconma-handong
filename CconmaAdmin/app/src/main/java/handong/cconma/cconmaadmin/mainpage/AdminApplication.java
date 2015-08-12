9f21bc533633cc0bab365e01bbd46b86122f57c4
ljs401 <irisluz@naver.com>
2015-08-12 15:11:39 +0900

    8/12 노티로 특정글 시작

--- CconmaAdmin/app/src/main/java/handong/cconma/cconmaadmin/mainpage/AdminApplication.java
+++ CconmaAdmin/app/src/main/java/handong/cconma/cconmaadmin/mainpage/AdminApplication.java
@@ -10,6 +10,8 @@ public class AdminApplication extends Application {
     private boolean board_search;
     private int tab_position;
     private static AdminApplication adminApplication;
+    private String gcm_board_no;
+    private String gcm_article_no;
 
     public static AdminApplication getInstance(){
         if( adminApplication == null ){
@@ -21,9 +23,17 @@ public class AdminApplication extends Application {
 
     public void setRefresh(boolean flag){ board_refresh = flag; }
 
+    public void setBoardNo(String boardNo){ gcm_board_no = boardNo; }
+
+    public void setArticleNo(String ArticleNo){ gcm_article_no = ArticleNo; }
+
     public boolean getRefresh(){ return board_refresh; }
 
     public void setTabPosition(int position){ tab_position = position; }
 
     public int getTabPosition(){ return tab_position; }
+
+    public String getBoardNo(){ return gcm_board_no; }
+
+    public String getArticleNo(){ return gcm_article_no; }
 }
